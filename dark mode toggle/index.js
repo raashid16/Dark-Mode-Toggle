9dfa2189edc70d913698776f2796b974a83774c1
RaashidAliD <61144031+raashid16@users.noreply.github.com>
2022-11-22 10:59:16 +0530

    Add files via upload

---
+++ dark mode toggle/index.js
@@ -0,0 +1,26 @@
+const inputEl = document.querySelector(".input");
+console.log(inputEl.checked);
+
+const bodyEl = document.querySelector("body");
+
+inputEl.checked=JSON.parse(localStorage.getItem("mode"));
+
+updateBody();
+
+function updateBody(){
+    if(inputEl.checked){
+        bodyEl.style.background = "black";
+
+    }else{
+        bodyEl.style.background = "white";
+    }
+}
+
+inputEl.addEventListener("input",()=>{
+    updateBody();
+    updatelocalStorage();
+})
+
+function updatelocalStorage(){
+    localStorage.setItem("mode",JSON.stringify(inputEl.checked));
+}
